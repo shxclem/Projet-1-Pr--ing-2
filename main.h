c10cf6d47fea153a5f29abdd0c3e070e98461fb0
VeNaGoD <104357218+VeNaGoD@users.noreply.github.com>
2023-02-03 00:17:52 +0100

    main.h final version

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
+#include <math.h>
 
 
 
@@ -17,9 +17,9 @@ typedef Chain* Pchain;  //Pchain = list (--tab)
 
 
 typedef struct abr{
-    Element elt;
-    char date;
-    int height;
+    int elt;
+    int secondelt;
+    float temp;
     struct chainon *fg;
     struct chainon *fd;
 }Abr;
@@ -27,10 +27,10 @@ typedef Abr* PABR;   //PABR = abr (--abr)
 
 
 
-typedef struct avl{ 
-    Element elt;
-    int height; 
-    int nelt;           //nelt = number of the same element
+typedef struct avl{
+    int elt;
+    int secondelt;
+    float temp;        
     struct avl *fg;
     struct avl *fd;
 }Avl;
@@ -38,6 +38,29 @@ typedef Avl* PAVL;  //PAVL = avl (--avl)
 
 
 
-Pchain create_Chain(int a,int b,int c);
-Pchain insert(Pchain pliste, int e, int sleep,float  temp);
+//all fonction for the list
+Pchain create_Chain(int a,int b,int c);                         
+Pchain insert(Pchain pliste, int e, int sleep,float temp);
+int sort_list(char **argv);
 
+
+
+//all fonction for the abr
+PABR create_abr(int a, int b, float c);
+PABR new_elt(PABR pa, int e, int sleep,float temp);
+void write_inorder(PABR a);
+void r_write_inorder(PABR a);
+int sort_abr(char **argv);
+
+
+
+//all fonction for the avl
+PAVL create_avl(int a, int b, float c);
+int height(PAVL avl);
+int balance(PAVL avl);
+PAVL rotate_left(PAVL avl);
+PAVL rotate_right(PAVL avl);
+PAVL insert_AVL(PAVL pa, int e, int sleep,float temp);
+void write_inorder_avl(PAVL a);
+void r_write_inorder_avl(PAVL a);
+int sort_avl(char **argv);

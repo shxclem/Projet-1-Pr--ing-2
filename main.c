06747a316022a5587d67bc18979c5df36069abd4
VeNaGoD <104357218+VeNaGoD@users.noreply.github.com>
2023-02-03 00:17:09 +0100

    main.c final version
     ouga ouga

--- main.c
+++ main.c
@@ -1,38 +1,21 @@
 #include "main.h"
 
 
+
 int main(int argc,char **argv){
-    if (argc < 6)return 1;         
-    if(strcmp(argv[3], "r")==0){               //compare if we need ascending or descending sort
-        if(strcmp(argv[4], "--tab")==0){        //compare wich sort we need
-            
-        }
-        else if(strcmp(argv[4], "--abr")==0){
-         
-        }
-        else if(strcmp(argv[4], "--avl")==0{
-        
-        }
-        else{
-            return 1;
-        }
-    }
+    if (argc<4)return 1;         
 
-
-
-    else{                                          //ascending sort
-        if(strcmp(argv[4], "--tab")==0){           //compare wich sort we need
+        if(strcmp(argv[3], "--tab")==0){        //compare wich sort we need
             sort_list(argv);
         }
-        else if(strcmp(argv[4], "--abr")==0){
-       
+        else if(strcmp(argv[3], "--abr")==0){
+            sort_abr(argv);
         }
-        else if(strcmp(argv[4], "--avl")==0{
-        
+        else if(strcmp(argv[3], "--avl")==0{
+            sort_avl(argv);
         }
         else{
             return 1;
         }
-    }
     return 0;
 }

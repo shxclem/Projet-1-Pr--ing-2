713d2f1ef49b188eccf4eab64009b82bf64ffab9
VeNaGoD <104357218+VeNaGoD@users.noreply.github.com>
2023-02-03 20:56:43 +0100

    final pour de vrai

--- sort_list.c
+++ sort_list.c
@@ -16,40 +16,54 @@ Pchain create_Chain(int a,int b,int c){     //create the chain for the list
 
 
 
-Pchain insert(char **argv, Pchain pliste, int e, int sleep,float temp){    //insertion of element in the list
+Pchain insert(int argc, char **argv, Pchain pliste, int e, int sleep,float temp){    //insertion of element in the list
     Pchain new= create_Chain(e,sleep,temp);
     Pchain p1 = pliste;
     if (p1 == NULL) {
         return new;
     }
-    if(strcmp(argv[5], "-r")==0){                                //compare with argument -r     
+    if(argc == 6 && strcmp(argv[5], "-r")==0){                                //compare with argument -r     
         while (p1->next != NULL && e <= p1->next->elt){
             p1=p1->next;
         }
+        if (new->elt > p1->elt) {
+            new->next = p1;
+            return new;
+        }
+        else {
+            new->next=p1->next;
+            p1->next=new;
+        }
     }
-    else if(strcmp(argv[5], "-r")!=0){
+    else {
         while (p1->next != NULL && e >= p1->next->elt){
             p1=p1->next;
         }
+        if (new->elt < p1->elt) {
+            new->next = p1;
+            return new;
+        }
+        else {
+            new->next=p1->next;
+            p1->next=new;
+        }
     }
-    else{
-        exit (1);
-    }
-    new->next=p1->next;
-    p1->next=new;
+
     return pliste;
 }
 
 
 
-int sort_list(char **argv){                           //sort with a list
+int sort_list(int argc, char **argv){                           //sort with a list
     Pchain list=NULL;
     FILE *fic=fopen(argv[1], "r");         //open file
     if(fic==NULL) exit(2);
     int ID;
-    char predate[30];
+    char predate[25];
     float temp;
-    while((fscanf(fic,"%d %s %f\n", &ID, predate, &temp)) != EOF){    //get the elmt, date and ID station
+    while((fscanf(fic,"%d,%[^,],%f\n", &ID, predate, &temp)) != EOF){    //get the elmt, date and ID station
+       printf("a\n");
+
         predate[19]='\0';                                           // cut the date to have only years, months, days, and hours
         struct tm tm;
         time_t date;
@@ -57,14 +71,15 @@ int sort_list(char **argv){                           //sort with a list
             date=mktime(&tm);
         }
         if(strcmp(argv[4], "-date")==0){          //compare argument to know wich data we will sort
-            list=insert(argv,list,date,ID,temp);
+            list=insert(argc, argv,list,date,ID,temp);
         }
         else if(strcmp(argv[4], "-id")==0){         
-            list=insert(argv,list,ID,date,temp);
+            list=insert(argc, argv,list,ID,date,temp);
         }
         else{
             return 1;                       //case error
         }
+
     }
     fclose(fic);                            //close file
     fic=fopen(argv[2], "w+");                                    //open file

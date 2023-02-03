c8df479d419ddf65aaa50d047baf83e19d44b14a
Clément VM <durecuclem@cy-tech.fr>
2023-02-03 01:35:14 +0100

    Correction du prototype

--- projet_wa.sh
+++ projet_wa.sh
@@ -1,7 +1,9 @@
 #!bin/bash 
  
-exist_csv=false
+exist_csv=false				
 exist_place=false
+exist_sort=false
+exist_para=false
 temp=0
 
 for arg in "$@"
@@ -14,24 +16,37 @@ do
 		echo "Too much .csv files among arguments"
 		exit 2	
 		
-	elif [[ ( "$arg" == "-F"  || "$arg" == "-G" || "$arg" == "-S" || "$arg" == "-A" || "$arg" == "-O" || "$arg" == "-Q" ) && "$exist_place" = true ]] ; then
+	elif [[ ( "$arg" == "-F"  || "$arg" == "-G" || "$arg" == "-S" || "$arg" == "-A" || "$arg" == "-O" || "$arg" == "-Q" ) && "$exist_place" = true ]] ; then   #The argument corresponds to a place and a place has already been written
 		echo "Too much places among arguments"
 		exit 2	
 
-	elif [[ ( "$arg" == "-F"  || "$arg" == "-G" || "$arg" == "-S" || "$arg" == "-A" || "$arg" == "-O" || "$arg" == "-Q" ) && "$exist_place" = false ]]; then
+	elif [[ ( "$arg" == "-F"  || "$arg" == "-G" || "$arg" == "-S" || "$arg" == "-A" || "$arg" == "-O" || "$arg" == "-Q" ) && "$exist_place" = false ]]; then   #The argument corresponds to a place and no place has been written
 		exist_place=true
 		place="$arg"
 		var[temp]="$arg"
 		temp=$temp+1
 		
-	else 			      					   #The argument is a not a .csv, we collect it
+	elif [[ ( "$arg" == "--tab" || "$arg" == "--abr" || "$arg" == "--avl" ) && "$exist_sort" = true ]]; then   #The argument corresponds to a type of sorting and a type of sorting has already been written
+		echo "Too much types of sorting among arguments"
+		exit 2
+
+	elif [[ ( "$arg" == "--tab" || "$arg" == "--abr" || "$arg" == "--avl" ) && "$exist_sort" = false ]]; then #The argument corresponds to a type of sorting and no type of sorting has already been written
+		exist_sort=true
+		sort="$arg"
+
+	elif [[ ( "$arg" == "-p1"  || "$arg" == "-p2" || "$arg" == "-p3" || "$arg" == "-t1" || "$arg" == "-t2" || "$arg" == "-t3" || "$arg" == "-w" || "$arg" == "-h" || "$arg" == "-m" ) ]]; then  #The argument is a decent parameter, we collect it	
+		exist_para=true
 		var[temp]="$arg"
 		temp=$temp+1
+	else 
+		echo "Parameters are wrong or missing"
+		exit 1
 	fi
 done
 
-if [ "$exist_csv" = true ]; then
+if [[ ( "$exist_csv" = true ) && ( "$exist_para" = true ) ]]; then		#Verifying that the program is based on a ressource file and atleast one paramater then displaying them
 	echo "$csv_file"
+	echo "$sort"
 	for (( i=0 ; i<$temp ; i++))
 	do
 		echo "${var[i]}"
@@ -39,11 +54,11 @@ if [ "$exist_csv" = true ]; then
 else
 	echo "No .csv has been written among arguments"
 	exit 1
-fi
+fi 
 
-case $place in
+case $place in					#Defining which IDs are we going to be interested about depending on which place has been given
 "-F")
-cond="/7558|7027/"
+cond="//"
 ;;
 "-O")
 cond="/61980|67005/"
@@ -59,18 +74,24 @@ cond="/78890|78897|78922|78925|78894/"
 ;;
 "-Q")
 cond="/61972|61976|61998/"
-*)
+;;
 esac
 
-tri="avl"
-gcc -o main main.c
+#gcc -o main main.c
 
-for (( j=0 ; j<$i ; j++))
+
+if [ -z "$sort" ]; then 					#If no type of sorting has been given, default one is used
+	sort=--avl
+fi
+
+echo "$sort"
+
+for (( j=0 ; j<$i ; j++))									#Deciding what to do depending on the parameters that have been given
 do
 	if [[ "${var[j]}" = "-t1" ]]; then
 		awk -F ","  'BEGIN {OFS=","} { print $1,$2,$12 }' input/$csv_file > fileT1m.csv
 		echo "fileT1m.csv generated"
-		./main fileT1m.csv exitT1m.csv --"$tri" -date
+		./main fileT1m.csv exitT1m.csv --"$sort" -date
 		
 		awk -F ","  'BEGIN {OFS=","} { print $1,$2,$13 }' input/$csv_file > fileT1M.csv
 		echo "fileT1M.csv generated"
@@ -126,7 +147,7 @@ do
 	fi
 
 	if [[ "${var[j]}" = "-h" ]]; then
-		awk -F ","  'BEGIN {OFS=","} {  if ($1 ~ /7558|7027/)  print $1,$2}' input/$csv_file > file2.csv
+		#awk -F ","  'BEGIN {OFS=","} {  if ($1 ~ /7558|7027/)  print $1,$2}' input/$csv_file > file2.csv
 		#awk -F ","  'BEGIN {OFS=","} { print $1,$2,$14 }' input/$csv_file > fileH.csv	
 		echo "fileH.csv generated"
 		#Exécuter le programme avec ordre décroissant -r 
